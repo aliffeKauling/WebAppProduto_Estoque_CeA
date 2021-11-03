9a117ca121407d13cbc32ea8b7146a0944e618fe
Danilo Menezes <dan.mene@hotmail.com>
2021-11-02 21:17:22 -0300

    Alterações tela de movimentação

---
+++ WebAppProduto/Pages/Produtos/Movimentacao.cshtml.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebAppProduto.Models;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+
+namespace WebAppProduto.Pages.Produtos
+{
+    public class MovimentacaoModel : PageModel
+    {
+        [BindProperty]
+        public Produto Produto { get; set; }
+        string baseUrl = "https://localhost:44369/";
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(baseUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(
+                    new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage response = await client.GetAsync("api/Produtos/" + id);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string result = response.Content.ReadAsStringAsync().Result;
+                    Produto = JsonConvert.DeserializeObject<Produto>(result);
+                }
+            }
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(baseUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(
+                    new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage response = await client
+                    .PutAsJsonAsync("api/Produtos/" + Produto.ID, Produto);
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToPage("./Feedbacks/Alterado");
+                }
+                else
+                {
+                    return Page();
+                }
+            }
+        }
+    }
+}
--- WebAppProduto/wwwroot/js/site.js
+++ WebAppProduto/wwwroot/js/site.js
@@ -5,7 +5,7 @@
 
 function getComboA(selectObject) {
     var value = selectObject.value;
-    window.location.replace("https://localhost:44310/Produtos/Edit?id=" + value);
+    window.location.replace("https://localhost:44310/Produtos/Movimentacao?id=" + value);
 }
 
 function urli() {
@@ -14,3 +14,47 @@ function urli() {
     var urli = "https://localhost:44310/Produtos/Edit?id=" + value;
     return urli;
 }
+
+    $(function () {
+        $(".details").click(function () {
+            var id = $(this).attr("data-id");
+            $("#modal").load("Produtos/Detail?id=" + id, function () {
+                $("#modal").modal();
+            })
+        });
+    })
+
+$(function () {
+    $(".movimentar").click(function () {
+        $("#modal-movimentar").load("Produtos/Movimentar", function () {
+            $("#modal-movimentar").modal();
+        })
+    });
+})
+
+function getComboB(selectObject) {
+    var value = selectObject.value;
+}
+
+function calcula() {
+    var tipoMovimentacao = document.getElementById("comboB");
+
+    if (tipoMovimentacao.value == 1) {
+
+        MovimentarEntrada()
+    } else {
+        MovimentarSaida()
+    }
+
+}
+
+function MovimentarEntrada() {
+    var novaquantidade = document.getElementById("novaquantidade");
+    document.getElementById('resultado').value = parseInt(resultado.value) + parseInt(novaquantidade.value);
+}
+
+function MovimentarSaida() {
+    var novaquantidade = document.getElementById("novaquantidade");
+    document.getElementById('resultado').value = parseInt(resultado.value) - parseInt(novaquantidade.value);
+}
+
